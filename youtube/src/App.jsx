487b2d552b6b62568762d87ea4cd6d85603ea35b
kimbumsoo0820 <kbs2082@gmail.com>
2021-08-18 11:12:08 +0900

    Update

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -5,36 +5,23 @@ import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 const selectVideo = (video) => {
   setSelectedVideo(video);
 };
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyA5WgCShKbBKTZZuAH2dAewZpli7YtvbAU`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
-
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   };
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA5WgCShKbBKTZZuAH2dAewZpli7YtvbAU", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
 
+
+  useEffect(() => {
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
