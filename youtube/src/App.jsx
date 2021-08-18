8c92b2a521118f3b4437ab6df4dcc03c32b3a7c7
kimbumsoo0820 <kbs2082@gmail.com>
2021-08-18 09:09:57 +0900

    Update

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -3,10 +3,27 @@ import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+const selectVideo = (video) => {
+  setSelectedVideo(video);
+};
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyA5WgCShKbBKTZZuAH2dAewZpli7YtvbAU`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
 
+  };
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -21,8 +38,11 @@ function App() {
   }, []);
   return (
     <div className={styles.app}>
-    <SearchHeader/>
-    <VideoList videos={videos} />
+    <SearchHeader onSearch={search}/>
+    {
+      selectedVideo && <VideoDetail video={selectedVideo}/>
+    }
+    <VideoList videos={videos} onVideoClick={selectVideo}/>
     </div>
     
   );
--- youtube/src/components/search_header/search_header.jsx
+++ youtube/src/components/search_header/search_header.jsx
@@ -1,14 +1,31 @@
-import styles from './search_header.moudle.css';
-import React from 'react';
+import styles from './search_header.module.css';
+import React, { useRef } from 'react';
 
-const SearchHeader = (props) => {
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef();
+
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        console.log(value);
+        onSearch(value);
+    }
+    const onClick = () => {
+        handleSearch();
+    }
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter') {
+            handleSearch();
+        }
+    }
             return (
                 <header className={styles.header}>
+                    <div className={styles.logo}>
                     <img className={styles.img} src="/images/logo.png" alt="logo" />
                     <h1 className={styles.title} >Youtube</h1>
-                    <input className={styles.input} type="search" placeholder="Search..." />
-                    <button className={styles.button} type="submit">
-                        <img src="images/search.png"/>
+                    </div>
+                    <input ref={inputRef} className={styles.input} type="search" placeholder="Search..." onKeyPress ={onKeyPress}/>
+                    <button className={styles.button} type="submit" onClick={onClick}>
+                        <img className={styles.buttonImg} src="images/search.png" alt="search"/>
                     </button>
                 </header>
             );
